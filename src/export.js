cd616547286cd4fc72fabcbe754b94c023e92565
Alexander Kit <source.kit@gmail.com>
2013-04-15 01:53:50 +0200

    [-] not needed comments

--- src/export.js
+++ src/export.js
@@ -1,8 +1,4 @@
 
-//////r.animate = Animate;
-//////r.animate.Model = Model;
-//////r.animate.sprite = Sprite;
-
 return {
 	animate: function(element, model, onend){
 		new Model(model).start(element, onend);
